989eaf5531fba151eca825371521cc0171a95d21
Jose <joselego129@gmail.com>
2023-03-14 11:39:46 -0500

    extend menus

--- clonesite/pages/static/js/dropdown.js
+++ clonesite/pages/static/js/dropdown.js
@@ -4,12 +4,18 @@ var bigmama = document.getElementsByTagName('html')[0];
     var blanketObject = document.getElementsByName('BLANKET')[0];
     var dropDownMenu = document.getElementsByClassName("js-main-nav-dropdown")[0];
     var listys = document.querySelectorAll('a[data-name="NAV_SUBSECTION_PARENT"]');
+    var TheReport = document.querySelectorAll('[data-name="NAV_DROPDOWN"]')[0];
+    var Textbook = document.querySelectorAll('[data-name="NAV_DROPDOWN"]')[1];
+    console.log(Textbook.childNodes);
     for (var i = 0; i < listys.length; i++) {
       listys[i].addEventListener('mouseenter', forSections);
     };
     dropDownMenu.addEventListener('mouseleave',hideThat);
     accesMenu.addEventListener('mouseenter',myNiceFunction);
-    removeMenu.addEventListener('mouseleave',myNiceFunction)
+    removeMenu.addEventListener('mouseleave',myNiceFunction);
+    TheReport.addEventListener('mouseenter',ReportOpen);
+    TheReport.addEventListener('mouseleave',ReportOpen);
+    Textbook.addEventListener('click',TextbookOpen);
     function myNiceFunction(){
       dropDownMenu.classList.toggle('is-open');
       bigmama.classList.toggle('nav-open');
@@ -17,7 +23,6 @@ var bigmama = document.getElementsByTagName('html')[0];
     };
     function forSections(thing){
       var otherOne = document.getElementsByClassName('is-selected');
-      console.log(otherOne);
       if (otherOne.length > 0){
         otherOne[0].hidden = true;
         otherOne[0].classList.toggle('is-selected');
@@ -31,4 +36,19 @@ var bigmama = document.getElementsByTagName('html')[0];
     function hideThat(){
       dropDownMenu.classList.toggle('has-visibile-subsection');
     }
-    console.log(listys)
+    function ReportOpen(thing){
+      thing.currentTarget.childNodes[3].classList.toggle('animate-fade-hidden');
+      thing.currentTarget.childNodes[3].classList.toggle('animate-fade-entered');
+      blanketObject.classList.toggle('animate-fade-hidden');
+      blanketObject.classList.toggle('animate-fade-entered');
+    }
+    function TextbookOpen(thing){
+      thing.currentTarget.childNodes[1].classList.toggle('animate-fade-hidden');
+      thing.currentTarget.childNodes[1].classList.toggle('animate-fade-entered');
+      thing.currentTarget.childNodes[3].classList.toggle('animate-fade-hidden');
+      thing.currentTarget.childNodes[3].classList.toggle('animate-fade-entered');
+      thing.currentTarget.childNodes[5].classList.toggle('animate-fade-hidden');
+      thing.currentTarget.childNodes[5].classList.toggle('animate-fade-entered');
+      blanketObject.classList.toggle('animate-fade-hidden');
+      blanketObject.classList.toggle('animate-fade-entered');
+    }
